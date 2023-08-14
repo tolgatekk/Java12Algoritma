33e524ba8f07497c07645a780f47738130d871dc
unknown <tolgatek75@gmail.com>
2023-08-14 14:16:20 +0300

    ProjeninOlusturulmasi

---
+++ src/lesson001/Question1.java
@@ -0,0 +1,16 @@
+package lesson001;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	System.out.println("Github Denemesi");
+	
+	//Class İsimleri PascalCase tarzında isimlendirilir
+	
+	
+	}
+
+}
